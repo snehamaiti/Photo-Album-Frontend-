e2de713bde4c4563d0e3f49ea1010fdbfba4dc76
snehamaiti <snehamaiti2001@gmail.com>
2021-02-16 21:01:43 +0530

    photo Gallery(frontend)

---
+++ Javascript/all.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    alert("Your page is loaded");
+  }
---
+++ Javascript/p2.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    alert("Welcome to category section");
+  }
+  function bigImg(x) {
+x.style.height = "150px";
+x.style.width = "150px";
+}
+
+function normalImg(x) {
+x.style.height = "100px";
+x.style.width = "100px";
+}
